65617822260646d3e546665a2be256ba1e5d82fb
erekle <erekle.k1@gmail.com>
2018-03-22 13:19:01 +0400

    verify google

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,11 +1,13 @@
 <!doctype html>
-<html lang="en">
+<html lang="{{app()->getLocale()}}">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Erekle Kereselidze - ერეკლე კერესელიძე</title>
+    <meta name=”description” content="მე ვარ ერეკლე კერესელიძე, ვებ დეველოპერი საქართველოდან.">
+    <meta name="google-site-verification" content="tx12YT7LpOrUAJYV80sLFL3mBkJ3NYiynC4zWRvJBOo" />
     <link rel="stylesheet" href="{{asset('css/index.css')}}">
     <script src="https://use.fontawesome.com/20aa2c3e8b.js"></script>
 </head>
